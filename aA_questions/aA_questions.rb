0fd48c8d78388498191d720f7ccf6fa895837d7c
ikesh pack <packappsforyou@gmail.com>
2019-06-10 13:54:26 -0400

    Created Question class and defined Question::all and Question#initialize(info) methods

--- aA_questions/aA_questions.rb
+++ aA_questions/aA_questions.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 require 'singleton'
 
-class QuestionsDBConnection < SQLite3::Database
+class QuestionDBConnection < SQLite3::Database
 
   include Singleton
 
@@ -10,5 +10,23 @@ class QuestionsDBConnection < SQLite3::Database
     self.type_translation = true
     self.results_as_hash = true
   end
+
+end
+
+class Question
+
+    attr_accessor :id, :body, :title, :author_id
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM questions")
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def initialize(info)
+        @id = info['id']
+        @title = info['title']
+        @body = info['body']
+        @author_id = info['author_id']
+    end
 
 end
